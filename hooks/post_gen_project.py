3e37799fe91bb6cd5b8b16eb982af6371fb35e27
Giuseppe Murro <giuseppe.murro@giorgioarmani.it>
2024-05-04 17:16:56 +0200

    add deploy dev

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import shutil
+from pathlib import Path
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
@@ -20,15 +21,18 @@ if __name__ == "__main__":
     if "{{cookiecutter.mkdocs}}" != "y":
         remove_dir("docs")
         remove_file("mkdocs.yml")
-        remove_dir("scripts")
+        remove_file(str(Path("script") / "gen_ref_pages.py"))
 
     if "{{cookiecutter.dockerfile}}" != "y":
         remove_file("Dockerfile")
+        if "{{cookiecutter.include_github_actions}}" == "y":
+            remove_file(str(Path(".github") / "workflows" / "deploy_dev.yml"))
+            remove_file(str(Path("script") / "versioning_script.sh"))
 
     if "{{cookiecutter.codecov}}" != "y":
         remove_file("codecov.yaml")
         if "{{cookiecutter.include_github_actions}}" == "y":
-            remove_file(".github/workflows/validate-codecov-config.yml")
+            remove_file(str(Path(".github") / "workflows" / "validate-codecov-config.yml"))
 
     if "{{cookiecutter.devcontainer}}" != "y":
         remove_dir(".devcontainer")
