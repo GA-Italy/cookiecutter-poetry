268faba6261f200b88262db3938888507ce9c378
Giuseppe Murro <giuseppe.murro@giorgioarmani.it>
2024-05-04 17:38:09 +0200

    pathlib not working

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import os
 import shutil
-from pathlib import Path
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
@@ -21,18 +20,18 @@ if __name__ == "__main__":
     if "{{cookiecutter.mkdocs}}" != "y":
         remove_dir("docs")
         remove_file("mkdocs.yml")
-        remove_file(str(Path("script") / "gen_ref_pages.py"))
+        remove_file("script/gen_ref_pages.py")
 
     if "{{cookiecutter.dockerfile}}" != "y":
         remove_file("Dockerfile")
         if "{{cookiecutter.include_github_actions}}" == "y":
-            remove_file(str(Path(".github") / "workflows" / "deploy_dev.yml"))
-            remove_file(str(Path("script") / "versioning_script.sh"))
+            remove_file(".github/workflows/deploy_dev.yml")
+            remove_file("script/versioning_script.sh")
 
     if "{{cookiecutter.codecov}}" != "y":
         remove_file("codecov.yaml")
         if "{{cookiecutter.include_github_actions}}" == "y":
-            remove_file(str(Path(".github") / "workflows" / "validate-codecov-config.yml"))
+            remove_file(".github/workflows/validate-codecov-config.yml")
 
     if "{{cookiecutter.devcontainer}}" != "y":
         remove_dir(".devcontainer")
